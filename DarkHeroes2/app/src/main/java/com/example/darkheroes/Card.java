33b0ee9062b3515afabe1e939fb3d33f6ea3b272
Lerto <Leva.kislov@mail.ru>
2016-03-09 20:17:04 +0700

    added

--- DarkHeroes2/app/src/main/java/com/example/darkheroes/Card.java
+++ DarkHeroes2/app/src/main/java/com/example/darkheroes/Card.java
@@ -5,30 +5,23 @@ public class Card {
     public int hp;
     public int mana;
     public int attack;
-    public String effect;
     public String clas;
-    public boolean inDeck;
     Card(){
-        this.inDeck = false;
         this.hp = 1;
         this.mana = 0;
         this.attack = 0;
         this.name = "Card";
-        this.effect = "";
         this.clas = "Basic";
     }
-    Card(int hp, int mana, int attack, String name, String effect) {
+    Card(String name, int mana, int hp, int attack) {
         this.hp = hp;
         this.mana = mana;
         this.attack = attack;
         this.name = name;
-        this.effect = effect;
-        this.inDeck = true;
     }
     public void doDamage(int oponattack) {
         this.hp = this.hp - oponattack;}
     int getHp(){return hp;};
     int getAttack(){return attack;};
     String getName(){return name; };
-    String getEffect(){return effect; };
 }
