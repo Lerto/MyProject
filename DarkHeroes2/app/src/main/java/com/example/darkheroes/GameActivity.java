572b5e71f9f2bef868f341d92accd1f9a8dcda73
Lerto <Leva.kislov@mail.ru>
2016-04-18 23:36:15 +0700

    been added

--- DarkHeroes2/app/src/main/java/com/example/darkheroes/GameActivity.java
+++ DarkHeroes2/app/src/main/java/com/example/darkheroes/GameActivity.java
@@ -2,10 +2,13 @@ package com.example.darkheroes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -15,10 +18,15 @@ public class GameActivity extends AppCompatActivity {
 
     Button[] butDeck = new Button[10];
     Button butMap[] = new Button[7];
+    Button butET;
+    int iDeck = 25;
+    TextView tv;
     int i = 0;
     int iCard = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
         butDeck[0] = (Button) findViewById(R.id.deckCard1);
         butDeck[1] = (Button) findViewById(R.id.deckCard2);
         butDeck[2] = (Button) findViewById(R.id.deckCard3);
@@ -36,32 +44,45 @@ public class GameActivity extends AppCompatActivity {
         butMap[4] = (Button) findViewById(R.id.mapCard5);
         butMap[5] = (Button) findViewById(R.id.mapCard6);
         butMap[6] = (Button) findViewById(R.id.mapCard7);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        tv = (TextView) findViewById(R.id.textViewDeck);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metricsB = new DisplayMetrics();
+        for(int i = 0; i < 7; i++){
+            butMap[i].setHeight(displayMetrics.heightPixels/10);
+            butMap[i].setWidth(displayMetrics.widthPixels/10);
+        }
+        for(int i = 0; i < 10; i++){
+            butDeck[i].setHeight(displayMetrics.heightPixels/8);
+            butDeck[i].setWidth(displayMetrics.widthPixels/12);
+        }
+        butET = (Button) findViewById(R.id.butEndTurn);
+        butET.setHeight(displayMetrics.heightPixels / 10);
+        butET.setWidth(displayMetrics.widthPixels / 10);
     }
     public void onClick(View v){
-       int s = Integer.parseInt(v.getTag().toString());
+        int s = Integer.parseInt(v.getTag().toString());
         s--;
-        Button plauBut = butMap[i];
-        Button deccBut = butDeck[s];
-        plauBut.setVisibility(View.VISIBLE);
-        plauBut.setText(deccBut.getText());
-
-        for(int j = s; j < iCard; j++){
-            butDeck[j] = butDeck[j+1];
+        if(i != 7){
+            Button plauBut = butMap[i];
+            Button deccBut = butDeck[s];
+            plauBut.setVisibility(View.VISIBLE);
+            plauBut.setText(deccBut.getText());
+            Random rn = new Random();
+            int i1 = rn.nextInt() % 20 + 1;
+            plauBut = butDeck[iCard];
+            plauBut.setVisibility(View.INVISIBLE);
+            plauBut.setText("Карта" + Integer.toString(Math.abs(i1)));
         }
-        Random rn = new Random();
-        int i = rn.nextInt();
-        i /= 10;
-        plauBut = butDeck[iCard];
-        plauBut.setVisibility(View.INVISIBLE);
-        plauBut.setText("Карта " + Integer.toString(i));
-        iCard--;
-        i++;
-
     }
     public void endTurn(View v) {
-    iCard++;
-        butDeck[iCard].setVisibility(View.VISIBLE);
+        if (iCard != 9 && iDeck != 0) {
+            iCard++;
+            butDeck[iCard].setVisibility(View.VISIBLE);
+        }
+        if(iDeck != 0){
+            iDeck--;
+            tv.setText("В вашей колоде: " + iCard + " карт");
+        }
     }
 }
--- DarkHeroes2/app/src/main/java/com/example/darkheroes/PlayActivity.java
+++ DarkHeroes2/app/src/main/java/com/example/darkheroes/PlayActivity.java
@@ -4,17 +4,29 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class PlayActivity extends AppCompatActivity {
 
+    Button but1;
+    Button but2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        but1 = (Button) findViewById(R.id.buttonDontOnly);
+        but2 = (Button) findViewById(R.id.buttonOnly);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metricsB = new DisplayMetrics();
+        but1.setWidth(displayMetrics.widthPixels / 2);
+        but2.setWidth(displayMetrics.widthPixels / 2);
     }
 
 
